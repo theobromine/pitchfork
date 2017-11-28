b4ed21bfba7f069c14268ca5f43830eb1f1bc67e
chinrw <chinqrw@gmail.com>
2017-11-27 19:27:43 -0500

    Statics work

--- Django/Django_Pycharm/webapp/urls.py
+++ Django/Django_Pycharm/webapp/urls.py
@@ -1,13 +1,15 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-]
+                  url(r'^$', views.index, name="index"),
+                  # ex: /polls/5/
+                  url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+                  # ex: /polls/5/results/
+                  url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+                  # ex: /polls/5/vote/
+                  url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
